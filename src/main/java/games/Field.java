5201649420dca58a79d07abc1f4bfbdc4b79d573
Mateusz Bieńkowski <ferumdriel@gmail.com>
2018-03-31 20:10:59 +0200

    implemented API Field class

--- src/main/java/games/Field.java
+++ src/main/java/games/Field.java
@@ -1,5 +1,28 @@
 package games;
 
 public class Field {
-    
+    private Pawn pawn;
+    private Marker currentMarker = Marker.FREE;
+
+    public boolean isFieldTaken() {
+        return pawn != null;
+    }
+
+    public void freeField(){
+        pawn = null;
+        currentMarker = Marker.FREE;
+    }
+
+    public void takeField(Pawn pawn){
+        this.pawn = pawn;
+        currentMarker = Marker.PAWN;
+    }
+
+    public Pawn getPawn() {
+        return pawn;
+    }
+
+    public char getFieldStatus(){
+        return currentMarker.getMark();
+    }
 }
