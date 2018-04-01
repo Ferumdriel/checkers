08b1ac22e34873fc681d6b8e1b03826c0be8b755
Mateusz Bieńkowski <ferumdriel@gmail.com>
2018-04-01 21:19:24 +0200

    added test coverage for MoveDirection.NE

---
+++ src/test/java/games/MoveValidatorTest.java
@@ -0,0 +1,28 @@
+package games;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveValidatorTest {
+    MoveValidator moveValidator;
+    Board board;
+    Pawn pawn;
+    @BeforeEach
+    void setUp() throws Exception{
+        board = new Board(8,8);
+        pawn = new Pawn();
+        board.placePawn(pawn, new Position(7,7));
+        moveValidator = new MoveValidator(board);
+    }
+
+    @Test
+    @DisplayName("Throws NotAllowedMove exception when trying to move out of board")
+    void whenTryingToMoveFurtherThanBoardThenThrowException() throws NotAllowedMove {
+        assertThrows(NotAllowedMove.class, () -> moveValidator.checkMove(MoveDirection.NE, pawn));
+    }
+
+}
